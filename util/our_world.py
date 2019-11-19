e1daa7b046348fd82f670c0d4f39790cab3f81bf
Dustin <clerin+gitkraken@gmail.com>
2019-11-19 15:11:49 -0800

    delete Rooms talbe then store new batch of rooms. refactored so creating and linking rooms is in a different function

--- util/our_world.py
+++ util/our_world.py
@@ -8,21 +8,28 @@ class CreateWorld:
 
         self.create_rooms()
 
+    def place_room(self, rooms_created, prev_room):
+        # pdb.set_trace()
+        room = Room(rooms_created, "A Generic Room", "This is a generic room.")
+        if prev_room != None:
+            room.s_to = int(prev_room.id)
+            prev_room.n_to = rooms_created
+            prev_room.save()
+            room.save()
+        prev_room = room
+        room.save()
+        rooms_created += 1
+        return {"rooms_created": rooms_created, "prev_room":prev_room}
+
+
     def create_rooms(self):
+        Room.objects.all().delete()
+
         rooms_to_create = self.num_rooms
         prev_room = None
-        rooms_created = 0
+        rooms_created = 1
         # pdb.set_trace()
         while rooms_to_create >= rooms_created:
-            # pdb.set_trace()
-            room = Room(rooms_created, "A Generic Room", "This is a generic room.")
-            if prev_room != None:
-                # print(f'CREATE CONNECTIONS FOR ROOM | PREV_ROOM: {prev_room.id}')
-                room.s_to = int(prev_room.id)
-                prev_room.n_to = rooms_created
-                prev_room.save()
-                room.save()
-            prev_room = room
-            # print(f"rooms_created: {rooms_created} | room id: {room.id} | prev_room: {prev_room}")
-            rooms_created += 1
-        # print('\n\n\n~~~~END OF THE LINE~~~\n\n\n')
+            returned_vals = self.place_room(rooms_created, prev_room)
+            rooms_created = returned_vals["rooms_created"]
+            prev_room = returned_vals["prev_room"]
