c28ce763ac24e70e959dff10d97800b67a161524
anthonypiazza <piazza.anthony.n@gmail.com>
2019-11-18 18:14:21 -0600

    Added Router to Urls

--- adv_project/urls.py
+++ adv_project/urls.py
@@ -1,8 +1,12 @@
+from rest_framework import routers
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import include
 
 from rest_framework.authtoken import views
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
