7497c3c4d0b3e36fe82ac507217f7cf1d904ce54
anthonypiazza <piazza.anthony.n@gmail.com>
2019-11-20 12:42:03 -0600

    Added Registration functionality - path to url.py

--- adv_project/urls.py
+++ adv_project/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/adv/', include('adventure.urls')),
-    path('api-token-auth/', views.obtain_auth_token)
+    path('api/login/', views.obtain_auth_token),
+    path('api/register/', include('rest_auth.urls'))
 ]
