b5f75f289dd222b8da70504f55715f281482c4ee
Dustin <clerin+gitkraken@gmail.com>
2019-11-20 11:14:52 -0800

    used the connectRooms method in the room object instead of what I had

--- util/our_world.py
+++ util/our_world.py
@@ -15,7 +15,7 @@ class CreateWorld:
             room = Room(rooms_created, f"This is room {rooms_created}", f"This is a generic room called {rooms_created}.")
             room.save()
             self.grid_view[(x,y)] = room.id
-            return {"rooms_created": rooms_created, "prev_room": room}
+            return {"prev_room": room}
 
         rm_dir = ["n", "e", "w", "s"]
         rvrm_dir = ["s", "w", "e", "n"]
@@ -31,11 +31,10 @@ class CreateWorld:
             
         if home_start and getattr(home, f"{check_dir}_to", 0) == 0:
             room = Room(rooms_created, f"This is room {rooms_created}", f"This is a generic room called {rooms_created}.")
-            setattr(home, f"{check_dir}_to", room.id)
-            setattr(room, f"{rvcheck_dir}_to", home.id)
-            self.grid_view[(x,y)] = room.id
             room.save()
-            home.save()
+            home.connectRooms(room, check_dir)
+            room.connectRooms(home, rvcheck_dir)
+            self.grid_view[(x,y)] = room.id
         elif getattr(prev_room, f"{check_dir}_to", 0) == 0:
             if (x,y) in self.grid_view:
                 rm_id = self.grid_view[(x,y)]
@@ -43,16 +42,15 @@ class CreateWorld:
                 return self.place_maze_with_validation(rooms_created, nxt_rm, home, False, x, y)
             else:
                 room = Room(rooms_created, f"This is room {rooms_created}", f"This is a generic room called {rooms_created}.")
-                setattr(prev_room, f"{check_dir}_to", room.id)
-                setattr(room, f"{rvcheck_dir}_to", prev_room.id)
-                self.grid_view[(x,y)] = room.id
-                prev_room.save()
                 room.save()
+                prev_room.connectRooms(room, check_dir)
+                room.connectRooms(prev_room, rvcheck_dir)
+                self.grid_view[(x,y)] = room.id
         else:
             rm_id = getattr(prev_room, f"{check_dir}_to", 0)
             nxt_rm = Room.objects.filter(id=rm_id)[0]
             return self.place_maze_with_validation(rooms_created, nxt_rm, home, False, x, y)
-        return {"rooms_created": rooms_created, "prev_room":room}
+        return {"prev_room":room}
 
 
     def create_rooms(self):
@@ -64,7 +62,6 @@ class CreateWorld:
         rooms_created = 1
         while rooms_to_create >= rooms_created:
             returned_vals = self.place_maze_with_validation(rooms_created, prev_room, home, True, 0, 0)
-            rooms_created = returned_vals["rooms_created"]
             prev_room = returned_vals["prev_room"]
             if rooms_created == 1:
                 home = prev_room
